895d6c3ed7d9d76cc45019772ba712c4dc885bbd
kalagoudap <43758241+kalagoudap@users.noreply.github.com>
2023-03-13 15:00:23 +0530

    Add files via upload

---
+++ src/test/java/com/java/hotelmanagemnt/HotelManagement/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.hotelmanagemnt.HotelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
